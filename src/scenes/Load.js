a08c1b08bf414caf822090b2c7c25f5ac73267e1
rozy-dixon <95520418+rozy-dixon@users.noreply.github.com>
2024-06-14 17:39:50 -0700

    temp enemy animations

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -7,20 +7,18 @@ class Load extends Phaser.Scene {
         // loading bar
 
         // ------------------------------------------------------------------------- LOADING ASSETS
-        // load tiles
+        // load images
         this.load.image('test-SheetPNG', './assets/sprites/png/test-Sheet.png')
-        //this.load.image('brick-SheetPNG', './assets/sprites/png/aqua_jade_brick.png')
         this.load.image('aqua_brickPNG', './assets/sprites/png/aqua_jade_brick.png')
         this.load.image('castle_wallsPNG', './assets/spritesheets/castle_walls_8x8.png')
         this.load.image('mossy_stonePNG', './assets/spritesheets/moss_block_8x8.png')
         this.load.image('rugPNG', './assets/spritesheets/rug.png')
         this.load.image('waterPNG', './assets/sprites/png/water.png')
-
-
-        //this.load.tilemapTiledJSON('test', './assets/tiled/test.tmj')
+        // load tilesheets
         this.load.tilemapTiledJSON('castle', './assets/tiled/castle.tmj')
-        // load spritesheet for wiz
+        // load spritesheets
         this.load.spritesheet('wiz', './assets/spritesheets/wiz-Sheet.png', { frameWidth: tileSize, frameHeight: tileSize })
+        this.load.spritesheet('temp', './assets/spritesheets/temp-Sheet.png', { frameWidth: tileSize, frameHeight: tileSize })
     }
 
     create() {
@@ -28,31 +26,18 @@ class Load extends Phaser.Scene {
         console.log('%cLOAD SCENE :^)', testColor)
         window.localStorage ? console.log('%cLocal storage supported by this cat! (^･･^=)~', goodColor) : console.log('%cLocal storage not supported by this cat ~(=^･･^)', badColor)
 
-        // create player character animation
-        this.anims.create({
-            key: 'down',
-            frames: this.anims.generateFrameNames('wiz', { start: 0, end: 7 }),
-            frameRate: 6,
-            repeat: -1
-        })
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNames('wiz', { start: 8, end: 15 }),
-            frameRate: 6,
-            repeat: -1
-        })
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNames('wiz', { start: 16, end: 23 }),
-            frameRate: 6,
-            repeat: -1
-        })
-        this.anims.create({
-            key: 'up',
-            frames: this.anims.generateFrameNames('wiz', { start: 24, end: 31 }),
-            frameRate: 6,
-            repeat: -1
-        })
+        // player character animations
+        this.anims.create({ key: 'down', frames: this.anims.generateFrameNames('wiz', { start: 0, end: 7 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'left', frames: this.anims.generateFrameNames('wiz', { start: 8, end: 15 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'right', frames: this.anims.generateFrameNames('wiz', { start: 16, end: 23 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'up', frames: this.anims.generateFrameNames('wiz', { start: 24, end: 31 }), frameRate: 6, repeat: -1 })
+        // temp enemy animations
+        this.anims.create({ key: 'green', frames: this.anims.generateFrameNames('temp', { start: 0, end: 1 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'purple', frames: this.anims.generateFrameNames('temp', { start: 2, end: 3 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'yellow', frames: this.anims.generateFrameNames('temp', { start: 4, end: 5 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'teal', frames: this.anims.generateFrameNames('temp', { start: 6, end: 7 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'blue', frames: this.anims.generateFrameNames('temp', { start: 8, end: 9 }), frameRate: 6, repeat: -1 })
+        this.anims.create({ key: 'pink', frames: this.anims.generateFrameNames('temp', { start: 10, end: 11 }), frameRate: 6, repeat: -1 })
 
         // move through
         this.scene.start('titleScene')
